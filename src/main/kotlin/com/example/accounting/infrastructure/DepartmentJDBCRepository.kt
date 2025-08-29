bc7414ff09a0b802c08c4c9682466618185f8f01
Ura <mikeneko41@icloud.com>
2025-08-29 18:31:32 +0900

    ロジック変更(インフラストラクチャーとユースケース)

---
+++ src/main/kotlin/com/example/accounting/infrastructure/DepartmentJDBCRepository.kt
@@ -0,0 +1,46 @@
+package com.example.accounting.infrastructure
+
+import com.example.accounting.domain.department.*
+import jooq.tables.Departments.DEPARTMENTS
+import org.jooq.DSLContext
+import org.springframework.stereotype.Repository
+
+@Repository
+class DepartmentJDBCRepository(
+    private val jooq: DSLContext,
+) : DepartmentRepository {
+    override fun find(code: DepartmentCode): Department? {
+        return jooq.selectFrom(DEPARTMENTS)
+            .where(DEPARTMENTS.DEPARTMENT_CODE.eq(code.value))
+            .fetchOne()
+            ?.let { departmentRecord ->
+                Department.reconstruct(
+                    DepartmentCode.of(departmentRecord[DEPARTMENTS.DEPARTMENT_CODE]),
+                    DepartmentName.of(departmentRecord[DEPARTMENTS.NAME]),
+                    departmentRecord[DEPARTMENTS.PARENT_DEPARTMENT_CODE]?.let { DepartmentCode.of(it) }
+                )
+            }
+    }
+
+    override fun list(): List<Department> {
+        return jooq.selectFrom(DEPARTMENTS)
+            .orderBy(DEPARTMENTS.DEPARTMENT_CODE, DEPARTMENTS.PARENT_DEPARTMENT_CODE)
+            .fetch()
+            .map { departmentRecord ->
+                Department.reconstruct(
+                    DepartmentCode.of(departmentRecord[DEPARTMENTS.DEPARTMENT_CODE]),
+                    DepartmentName.of(departmentRecord[DEPARTMENTS.NAME]),
+                    departmentRecord[DEPARTMENTS.PARENT_DEPARTMENT_CODE]?.let { DepartmentCode.of(it) }
+                )
+            }
+    }
+
+    override fun insert(department: Department) {
+        jooq.insertInto(DEPARTMENTS)
+            .set(DEPARTMENTS.ACCOUNT_CODE, department.code.value)
+            .set(DEPARTMENTS.NAME, department.name.value)
+            .set(DEPARTMENTS.ACCOUNT_TYPE, department.departmentType.toString())
+            .set(DEPARTMENTS.PARENT_ACCOUNT_CODE, department.parentCode?.value)
+            .execute()
+    }
+}
